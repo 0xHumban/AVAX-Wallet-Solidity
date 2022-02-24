e2d139519e3dfa508cb33e050f59f341bb46e067
Humbert Evan <evan.humbert@outlook.com>
2022-02-24 21:09:07 +0100

    Update Owner + Wallet
    Add a freeze
    getTotalBalance()

--- Contracts/Owner.sol
+++ Contracts/Owner.sol
@@ -3,9 +3,16 @@ pragma solidity ^0.8.7;
 contract Owner {
 
     address owner;
+    bool freeze;
 
     constructor() {
         owner = msg.sender;
+        freeze = false;
+    }
+
+    modifier isfrozen() {
+        require(freeze == false,"Wallets are currently frozen");
+        _;
     }
 
     modifier isOwner() {
--- Contracts/Wallet.sol
+++ Contracts/Wallet.sol
@@ -16,23 +16,40 @@ contract Wallet is Owner {
 
     mapping(address => Balance) Wallets;
 
-    function getBalance() public view returns(uint) {
+
+    function setupFreeze(bool _value) public Owner.isOwner(){
+        Owner.freeze = _value;
+    }
+
+    function getFreeze() public view returns(bool) {
+        return Owner.freeze;
+    }
+
+
+    function getTotalBalance() public Owner.isOwner() view returns(uint) {
         return address(this).balance;
     }
 
-    function withdrawAllmoney(address payable _to) public {
+    function getBalance() public view returns(uint) {
+        return Wallets[msg.sender].totalBalance;
+    }
+
+
+    function withdrawAllmoney(address payable _to) public Owner.isfrozen() {
         uint amount = Wallets[msg.sender].totalBalance;
         Wallets[msg.sender].totalBalance = 0;
         _to.transfer(amount);
     }
 
-    function withdrawMoney(address payable _to, uint _amount) public {
+
+    function withdrawMoney(address payable _to, uint _amount) public Owner.isfrozen() {
         require(_amount <= Wallets[msg.sender].totalBalance,"Not enought funds");
         Wallets[msg.sender].totalBalance -= _amount;
         _to.transfer(_amount);
     }
 
-    receive() external payable {
+
+    receive() external payable Owner.isfrozen() {
         Payment memory thisPayement = Payment(msg.value, block.timestamp);
         Wallets[msg.sender].totalBalance += msg.value;
         Wallets[msg.sender].payements[Wallets[msg.sender].numPayements] = thisPayement;
